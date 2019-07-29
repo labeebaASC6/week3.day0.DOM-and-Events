7524e12b02634f2b02bf3019b0bf36fac7749a68
unknown <labeebabrar222@gmail.com>
2019-07-29 14:08:29 -0400

    renamed to Frostfang and changed image

---
+++ renameIt/lib/js/main.js
@@ -0,0 +1,5 @@
+const imgElement = document.querySelector("#item_img");
+const h1Element = document.querySelector("#item_name");
+
+imgElement.src = "assets/Frostfang_item.png";
+h1Element.innerHTML = "Frostfang";
